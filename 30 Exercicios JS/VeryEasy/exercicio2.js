ab7fed59f68c70cf880549592747ca59c59cf4e1
Eduardo Franco <efs2437@gmail.com>
2024-03-13 19:38:25 -0300

    Delete 30 Exercicios JS directory

--- 30 Exercicios JS/VeryEasy/exercicio2.js
+++
@@ -1,8 +0,0 @@
-// Procedimento Recursivo I || chuck-chuck
-
-const repeat = (n) => "chuck-".repeat(n).slice(0, -1);
-
-console.log(repeat(4))
-console.log(repeat(1))
-console.log(repeat(8))
-console.log(repeat(2))
